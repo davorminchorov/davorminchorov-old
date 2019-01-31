fad62b3d76d3213433f207b5d245f5958168d33f
Davor Minchorov <davorminchorov@gmail.com>
2019-01-31 03:36:49 +0100

    Design the home page

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="min-h-screen flex py-10 flex-col px-6 md:px-16 lg:px-24">
-        <header class="flex flex-row items-center justify-around mb-10">
+        <header class="container mx-auto mb-10">
             <a href="/" class="no-underline text-grey text-2xl font-bold uppercase">Davor <span class="text-green">Minchorov</span></a>
-            <nav>
-                <a href="#" class="no-underline text-lg text-grey hover:text-green uppercase font-semibold tracking-wide text-xs pl-6">Resume</a>
-                <a href="#" class="no-underline text-lg text-grey hover:text-green uppercase font-semibold tracking-wide text-xs pl-6">Articles</a>
-                <a href="#" class="no-underline text-lg text-grey hover:text-green uppercase font-semibold tracking-wide text-xs pl-6">Contact</a>
-            </nav>
+            <!--<nav>-->
+                <!--<a href="#" class="no-underline text-lg text-grey hover:text-green uppercase font-semibold tracking-wide text-xs pl-6">Resume</a>-->
+                <!--<a href="#" class="no-underline text-lg text-grey hover:text-green uppercase font-semibold tracking-wide text-xs pl-6">Articles</a>-->
+                <!--<a href="#" class="no-underline text-lg text-grey hover:text-green uppercase font-semibold tracking-wide text-xs pl-6">Contact</a>-->
+            <!--</nav>-->
         </header>
 
-        <main class="flex-grow">
+        <main class="flex-grow container mx-auto mb-10">
             <router-view></router-view>
         </main>
 
