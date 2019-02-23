43fd4ac8988765ee26b410c64cd71cd0d9c98ba9
Davor Minchorov <davorminchorov@gmail.com>
2019-02-23 17:44:22 +0100

    Make adjustments to the image

--- resources/js/components/pages/Home.vue
+++ resources/js/components/pages/Home.vue
@@ -3,7 +3,7 @@
         <div class="flex flex-col justify-around">
             <div class="flex flex-col lg:flex-row bg-green text-white border-2 rounded border-green">
                 <div class="lg:w-1/2 lg:p-10 p-6 pb-0  flex justify-center">
-                    <img class="=rounded lg:w-64 lg:h-64 w-32 h-32"
+                    <img class="rounded-full lg:w-48 lg:h-48 w-32 h-32"
                          src="https://avatars3.githubusercontent.com/u/6518995?s=274&v=4" alt="Davor Minchorov Photo">
                 </div>
 
