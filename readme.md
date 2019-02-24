23f8c6898dbcc83b2ba145f32ef75ec52fdd20cf
Davor Minchorov <davorminchorov@gmail.com>
2019-02-24 16:49:47 +0100

    Add gradient to the green background

--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 # Davor Minchorov
 
 ## Davor Minchorov's personal website.
+
+### Things to do next:
+- [x] Add a gradient to the green background
+- [ ] Replace the technology stack icons with text
+- [ ] Design the login form
+- [ ] Implement the Login API
+- [ ] Integrate the Login API with the login form
--- resources/js/components/pages/Home.vue
+++ resources/js/components/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="flex flex-col justify-around">
-            <div class="flex flex-col lg:flex-row bg-green text-white border-2 rounded border-green">
+            <div class="gradient-green flex flex-col lg:flex-row text-white rounded">
                 <div class="lg:w-1/2 lg:p-10 p-6 pb-0  flex justify-center">
                     <img class="rounded-full lg:w-48 lg:h-48 w-32 h-32"
                          src="https://avatars3.githubusercontent.com/u/6518995?s=274&v=4" alt="Davor Minchorov Photo">
