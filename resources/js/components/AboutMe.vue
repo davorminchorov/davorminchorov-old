f6d03da43b8e760133315fa041363c521509e34c
Davor Minchorov <davorminchorov@gmail.com>
2019-07-05 10:40:43 +0200

    Redesign the layout of the home page

--- resources/js/components/AboutMe.vue
+++ resources/js/components/AboutMe.vue
@@ -1,14 +1,23 @@
 <template>
     <div>
-        <h3 class="text-gray-500 uppercase font-semibold text-xl">About <span class="text-green-500">Me</span></h3>
+        <h3 class="text-gray-500 uppercase font-semibold text-xl text-center">About <span class="text-green-500">Me</span></h3>
         <div class="pt-4 pr-10 text-md">
             I am a {{ age() }} old web developer from Skopje, Macedonia, who studied software engineering at <a href="http://www.uacs.edu.mk/home/" target="_blank" rel="noreferrer" class="font-semibold text-green-500 hover:text-green-600 ">University American College Skopje</a> and have more than {{ yearsOfExperience() }} of experience building custom web based solutions.
             <br>
             <br>
-            I am currently working as a full time Senior PHP Developer at <a href="https://adevait.com/" target="_blank" rel="noreferrer" class="font-semibold text-green-500 hover:text-green-600 ">Adeva</a> since 2018 and Mentor at <a href="https://femalebootcamp.adevait.com/" target="_blank" rel="noreferrer" class="font-semibold text-green-500 hover:text-green-600 ">Adeva's Female Bootcamp 2018</a>.
+            I am currently working as a Senior PHP Developer at <a href="https://adevait.com/" target="_blank" rel="noreferrer" class="font-semibold text-green-500 hover:text-green-600 ">Adeva</a> since January 2018.
             <br>
             <br>
-            Previously, I worked at <a href="http://tabellarius.mk" target="_blank" rel="noreferrer" class="font-semibold text-green-500 hover:text-green-600 ">Tabellarius</a> (2015), New Media Corp (2016) and <a href="http://www.intertec.io/" target="_blank" rel="noreferrer" class="font-semibold text-green-500 hover:text-green-600 ">Intertec</a> as part of the <a href="http://routyx.com/" target="_blank" rel="noreferrer" class="font-semibold text-green-500 hover:text-green-600 ">Routyx</a> team (2017).
+            Previously, I worked at:
+            <br>
+            - <a href="http://tabellarius.mk" target="_blank" rel="noreferrer" class="font-semibold text-green-500 hover:text-green-600 ">Tabellarius</a> (June 2015 - March 2016),
+            <br>
+            - New Media Corp (April 2016 - February 2017)
+            <br>
+            - <a href="http://www.intertec.io/" target="_blank" rel="noreferrer" class="font-semibold text-green-500 hover:text-green-600 ">Intertec</a> as part of the <a href="http://routyx.com/" target="_blank" rel="noreferrer" class="font-semibold text-green-500 hover:text-green-600 ">Routyx</a> team (March 2017 - December 2017).
+            <br>
+            <br>
+            Also, I was a mentor at <a href="https://femalebootcamp.adevait.com/" target="_blank" rel="noreferrer" class="font-semibold text-green-500 hover:text-green-600 ">Adeva's Female Bootcamp 2018</a>.
             <br>
             <br>
             If you want to contact me, you can do that by using the contact form below.
--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="min-h-screen flex flex-col lg:py-10 py-3 px-3 md:px-16 lg:px-24">
+    <div class="min-h-screen flex flex-col lg:py-10 py-3">
         <dm-navigation></dm-navigation>
 
-        <main class="flex-grow container mx-auto mb-10">
+        <main class="flex-grow mb-10">
             <router-view></router-view>
         </main>
 
