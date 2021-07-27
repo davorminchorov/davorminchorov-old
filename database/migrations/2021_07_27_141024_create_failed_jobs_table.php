372cd87c4a93e9d204978777569219eb151734d7
Davor Minchorov <davorminchorov@gmail.com>
2021-07-27 14:16:54 +0200

    Update the failed jobs table with a new migration

--- database/migrations/2021_07_27_141024_create_failed_jobs_table.php
+++ database/migrations/2021_07_27_141024_create_failed_jobs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOldFailedJobsTable extends Migration
+class CreateFailedJobsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class CreateOldFailedJobsTable extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists('failed_jobs');
+
         Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->id();
+            $table->string('uuid')->unique();
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
